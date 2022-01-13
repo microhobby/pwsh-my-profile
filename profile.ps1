32c4862052c087f4f1f8517b7587afd72ff370e1
Matheus Castello <matheus@castello.eng.br>
2022-01-13 01:03:55 -0300

    profile: Improve
    - Improve formatting
    - Improve Linux Windows Sections
    - Add get top process using the CPU
    - Add the subscribers section
    - Add the Operating system section
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- profile.ps1
+++ profile.ps1
@@ -5,37 +5,218 @@
 # Aliases
 #Set-Alias ls /usr/bin/ls --color=auto
 Set-Alias ls Get-ChildItem
-Set-Alias code code-insiders 
+#Set-Alias code code-insiders 
 Set-Alias grep "Select-String"
-#Set-Alias wget Invoke-WebRequest
+Set-Alias wget Invoke-WebRequest
 
-function gtc ()
-{
+function gtc () {
     git commit -vs
 }
 
-function gts ()
-{
+function gts () {
     git status
 }
 
-function gtd ()
-{
+function gtd () {
     git diff
 }
 
-function gta ()
-{
+function gta () {
     git add $args
 }
 
-function gtca ()
-{
+function gtca () {
     git commit --amend
+}
+
+<#
+.SYNOPSIS
+    git remote -v [list all the remotes in a verbose way]
+#>
+function gtr () {
+    git remote -v
+}
+
+function gtl () {
+    git log
+}
+
+function gtlo () {
+    git log --oneline
+}
+
+function gtp () {
+    git push $args
+}
+
+function gtpo () {
+    $gitRet = git rev-parse --abbrev-ref HEAD
+    git push origin $gitRet $args
+}
+
+<#
+.SYNOPSIS
+    git reset HEAD -- $args [reset the files in $args]
+#>
+function gtrs () {
+    git checkout HEAD -- $args
+}
+
+<#
+.SYNOPSIS
+    git cherry-pick $args [LE AWESOME 🍒 PICK]
+#>
+function gtcp () {
+    git cherry-pick $args
+}
+
+<#
+.SYNOPSIS
+    git checkout -b $args [Create a new branch]
+#>
+function gtcb () {
+    git checkout -b $args
+}
+
+<#
+.SYNOPSIS
+    git rebase -i HEAD~$args [git rebase interactive]
+#>
+function gtrb () {
+    git rebase -i HEAD~$args
+}
+
+<#
+.SYNOPSIS
+    git rebase --continue
+#>
+function gtrbc () {
+    git rebase --continue
+}
+
+<#
+.SYNOPSIS
+    git rebase --abort
+#>
+function gtrba () {
+    git rebase --abort
+}
+
+function exec () {
+    bash -c "exec $args"
+}
+
+function dell () {
+    ssh castello@192.168.15.53
+}
+
+<#
+.SYNOPSIS
+    dfimage -sV=1.36 nginx:latest [try to recover a dockerfile from docker image]
+#>
+function dfimage () {
+    docker run `
+        -v /var/run/docker.sock:/var/run/docker.sock `
+        --rm alpine/dfimage @args
+    # dfimage -sV=1.36 nginx:latest
+}
+
+function Test-CommandExists {
+
+ Param ($command)
+
+ $oldPreference = $ErrorActionPreference
+
+ $ErrorActionPreference = ‘stop’
+
+ try {if(Get-Command $command){RETURN $true}}
+
+ Catch { RETURN $false }
+
+ Finally {$ErrorActionPreference=$oldPreference}
+
+}
+
+function CustomHelp () {
+    try {
+        $line = $cursor = $null
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState(
+            [ref] $line,
+            [ref] $cursor
+        )
+
+        $helpDesc = "No help for you today 😥"
+        $helpDesc = $helpDesc.PadRight($Host.UI.RawUI.BufferSize.Width - 6, " ")
+
+
+        if ($line -like "* *" -or $line -like "./*" -or $line -like ".\*") {
+            ClearCustomHelp
+            return
+        }
+        else {
+            # TODO: find a way to get the man from Linux
+            if ($Global:IsLinux) {
+                $help = $(man -f $line 2>&1)
+                if ($help -like "*nothing appropriate*" -and (Test-CommandExists($line) -eq $true)) {
+                    $help = Get-Help $line
+                } elseif ($help -like "*nothing appropriate*") {
+                    $help = "No help for you today 😥"
+                }
+            } else {
+                if (Test-CommandExists($line) -eq $true) {
+                    $help = Get-Help $line
+                }
+            }
+        }
+
+        if ($null -ne $help.Synopsis -and $help.Count -le 1) {
+            #$helpDesc = $line
+            $help = $help.Synopsis.Trim().Replace("`n", " ")
+            $help = $help.Substring(0, [System.Math]::Min($Host.UI.RawUI.BufferSize.Width - 6, $help.length))
+            $help = $help.PadRight($Host.UI.RawUI.BufferSize.Width - 6, " ")
+            $helpDesc = $help
+        } elseif ($Global:IsLinux) {
+            # man
+            $help = $help.Split("`n")[0]
+            $help = $help.Substring(0, [System.Math]::Min($Host.UI.RawUI.BufferSize.Width - 6, $help.length))
+            $help = $help.PadRight($Host.UI.RawUI.BufferSize.Width - 6, " ")
+            $helpDesc = $help
+        }
+
+        $oldPosition = $Host.UI.RawUI.CursorPosition
+        $maxY = $Host.UI.RawUI.BufferSize.Height - 1
+        #$maxY = $oldPosition.Y + 6
+        $Host.UI.RawUI.CursorPosition = @{ X = 0; Y = $maxY }
+
+        Write-Host `
+            -ForegroundColor White `
+            -BackgroundColor DarkMagenta `
+            -NoNewline "🆘➡️ $helpDesc "
+        $Host.UI.RawUI.CursorPosition = $oldPosition
+    }
+    catch {
+        # do nothing
+        Write-Error $Error[0]
+    }
+}
+
+function ClearCustomHelp {
+    $help = ""
+    $helpDesc = $help.PadRight($Host.UI.RawUI.BufferSize.Width, " ")
+    $oldPosition = $Host.UI.RawUI.CursorPosition
+    $maxY = $Host.UI.RawUI.BufferSize.Height - 1
+    #$maxY = $oldPosition.Y + 6
+    $Host.UI.RawUI.CursorPosition = @{ X = 0; Y = $maxY }
+
+    Write-Host `
+        -NoNewline "$helpDesc"
+    $Host.UI.RawUI.CursorPosition = $oldPosition
 }
 
 # autocomplete
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Key Alt+i -ScriptBlock ${function:CustomHelp}
+Set-PSReadLineKeyHandler -Key Escape -ScriptBlock ${function:ClearCustomHelp}
 # Autocompletion for arrow keys
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
@@ -50,10 +231,11 @@ $ERROR_EMOJI = "😖", "😵", "🥴", "😭", "😱", "😡", "🤬", "🙃", "
 
 $global:EC = 0
 $global:EXIT_CODE = 0
+$Global:JobTop = $null
 
 # clear the last win32 exit code
 $global:LASTEXITCODE = 0
-$global:MINUS_SECTS = 2
+$global:MINUS_SECTS = 4
 
 # set my powerline blocks
 [System.Collections.Generic.List[ScriptBlock]]$Prompt = @(
@@ -84,27 +266,114 @@ $global:MINUS_SECTS = 2
     # git modified
     {
         $diff = git status -s -uno | Measure-Object | `
-                    Select-Object count -ExpandProperty Count
+            Select-Object count -ExpandProperty Count
 
         if ( $diff ) {
             " 📑 :: $diff "
-        } else {
+            $Global:Prompt.Colors[0] = "#b84a1c"
+        }
+        else {
             "."
+            $Global:Prompt.Colors[0] = "#2b2b2b"
         }
     }
     # my current path
     {
-        $CPATH = $executionContext.SessionState.Path.CurrentLocation.ToString().Split("/")
-        -join(" 📂 ", $CPATH[$CPATH.Count -2], "/", $CPATH[$CPATH.Count -1], " ")
+        if ($Global:IsWindows) {
+            $CPATH = $executionContext.SessionState.Path.CurrentLocation.ToString().Split("\")
+            -join (" 📂 ", $CPATH[$CPATH.Count - 2], "\", $CPATH[$CPATH.Count - 1], " ")
+        }
+        else {
+            $CPATH = $executionContext.SessionState.Path.CurrentLocation.ToString().Split("/")
+            -join (" 📂 ", $CPATH[$CPATH.Count - 2], "/", $CPATH[$CPATH.Count - 1], " ")
+        }
     }
     {
         "`t"
     }
     {
         if ($Global:IsWindows) {
-            "🪟 Windows"
+            $versions = [System.Environment]::OSVersion.Version
+            $build = $versions.Build
+            "🪟 Windows Build $build"
+        }
+        else {
+            $distro = ([String](cat /etc/issue)).Split(" ");
+            $name = $distro[0]
+            $version = $distro[1]
+            "🐧 Linux @$name $version"
+        }
+    }
+    {
+        #$subs = Invoke-RestMethod `
+        #-Uri "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC431MbbedNKuIuDBPeSCdyg&key=AIzaSyDTVZpGp9vZBMe_juODjYXdKomWEl7aDDU"
+        
+        #$subs = $subs.items[0].statistics.subscriberCount
+        # "🧮 ${subs} "
+        "🧮 3600"
+        $Global:Prompt.Colors[3] = "#800f55"
+    }
+    {
+        if ($Global:IsLinux -eq $false) {
+            # so we have something that is not idle
+            if (($Global:JobTop -ne $null) -and ($Global:JobTop.State -eq "Completed")) {
+                $topProcess = Receive-Job -Job $Global:JobTop
+                Remove-Job $Global:JobTop
+                $Global:JobTop = $null
+                $toptop = "💤"
+
+                foreach ($item in $topProcess) {
+                    if ($item.InstanceName.Contains("idle") -or
+                        $item.InstanceName.Contains("_total") -or
+                        $item.InstanceName.Contains("pwsh")
+                    ) {
+                        # continue
+                    }
+                    else {
+                        $toptop = $item.InstanceName
+                        "⚠️ ${toptop}"
+                        $Global:Prompt.Colors[4] = "#b84a1c"
+                        break
+                    }
+                }
+
+                if ($toptop.Contains("💤")) {
+                    "💤"
+                    $Global:Prompt.Colors[4] = "#187823"
+                }
+            }
+            elseif ($Global:JobTop -eq $null) {
+                $Global:JobTop = Start-Job -ScriptBlock {
+                    $NumberOfLogicalProcessors = (Get-WmiObject -class Win32_processor
+                        | Measure-Object -Sum NumberOfLogicalProcessors
+                    ).Sum
+                    $topProcess = (Get-Counter '\Process(*)\% Processor Time').Countersamples
+                    | Where-Object cookedvalue -gt ($NumberOfLogicalProcessors * 5)
+                    | Sort-Object cookedvalue -Desc
+  
+                    return $topProcess
+                }
+
+                "💤"
+                $Global:Prompt.Colors[4] = "#187823"
+            }
+            else {
+                "💤"
+                $Global:Prompt.Colors[4] = "#187823"
+            }
         } else {
-            "🐧 Linux"
+            $psret = $(ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu -o '|%c;%C' | head -n 2)
+            $pts = $psret.Split("`n")[1].Split("|")[1].split(";")
+            $cpu = $pts[1]
+            $cmd = $pts[0].Trim()
+
+            if ([System.Double]::Parse($cpu.Trim()) -gt 30) {
+                "⚠️ ${cmd}"
+                $Global:Prompt.Colors[4] = "#b84a1c"
+            } else {
+                "💤"
+                $Global:Prompt.Colors[4] = "#187823"
+            }
         }
     }
     { "`n" } # Start another line
@@ -120,28 +389,29 @@ $global:MINUS_SECTS = 2
             
             if ( $gitRet ) {
                 " &#xE0A0; $gitRet "
-                $Global:Prompt.Colors[3 - $global:MINUS_SECTS] = "#303030"
-            # not git repo and the last cmd return a error
-            } else {
+                # not git repo and the last cmd return a error
+            }
+            else {
                 # get a ramdom emoji
                 $ERROR_EMOJI[(Get-Random -Maximum $ERROR_EMOJI.count)]
             }
 
-            $Global:Prompt.Colors[5 - $global:MINUS_SECTS] = "#303030"
-            $Global:Prompt.Colors[7 - $global:MINUS_SECTS] = "Red"
+            $Global:Prompt.Colors[5] = "#821616"
+            $Global:Prompt.Colors[7] = "#821616"
         
-        # not git repo and all clear
-        } else {
+            # not git repo and all clear
+        }
+        else {
             if ( $gitRet ) {
                 " &#xE0A0; $gitRet "
-                $Global:Prompt.Colors[3 - $global:MINUS_SECTS] = "#303030"
-            # not git repo and the last cmd return a error
-            } else {
+                # not git repo and the last cmd return a error
+            }
+            else {
                 "👌"
             }
 
-            $Global:Prompt.Colors[5 - $global:MINUS_SECTS] = "#046D03"
-            $Global:Prompt.Colors[7 - $global:MINUS_SECTS] = "#303030"
+            $Global:Prompt.Colors[5] = "#187823"
+            $Global:Prompt.Colors[7] = "#187823"
         }
         
         # clear the errors and last exit code for the next interaction
@@ -149,118 +419,155 @@ $global:MINUS_SECTS = 2
         $global:LASTEXITCODE = 0
     }
     # my user name
-    { " castello 💉" }
+    { " castello 🤔" }
     # pipe
     { ">" * ($nestedPromptLevel + 1) }
 )
 
 # execute
 Set-PowerLinePrompt `
-     -PowerLineFont `
-     -SetCurrentDirectory `
-     -RestoreVirtualTerminal `
-     -HideError `
-     -Colors "#303030", "#303030", "#303030", "#046D03", "#0087ff", "#303030"
-
-# Linux environment
-# enable color support of ls and also add handy aliases
-if ( Test-Path -Path /usr/bin/dircolors ) {
-    test -r ~/.dircolors && $(dircolors -b ~/.dircolors | Out-Null) || $(dircolors -b | Out-Null)
-    
-    function l {
-        /usr/bin/ls --color=auto $args
-    }
-    #alias dir='dir --color=auto'
-    #alias vdir='vdir --color=auto'
-
-    # function grep {
-    #     /usr/bin/grep --color=auto $args
-    # }
-    function fgrep {
-        /usr/bin/fgrep --color=auto $args
-    }
-    function egrep {
-        /usr/bin/egrep --color=auto $args
-    }
-}
-
-# colored GCC warnings and errors
-#$env:GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
-
-# some more ls aliases
-function ll { ls -alF $args }
-function la { ls -A $args }
-function lcf { ls -CF $args }
-
-# bash completation?
-# Install-Module -Name "PSBashCompletions"
-function __completation ($command) {
-    Register-BashArgumentCompleter "$command" /usr/share/bash-completion/completions/$command
-}
-
-# register on demand
-Set-PSReadLineKeyHandler -Key Spacebar -ScriptBlock {
-    $line = $cursor = $null
-    
-    # get the command
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref] $line, [ref] $cursor)
-    # the key handled must be outputed
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert(' ')
-    
-    # check if we have the bash complestions mathc
-    try {
-        __completation $line   
-    }
-    catch {
-        # let's clear these errors
-        $Error.RemoveAt(0)
-        $Error.RemoveAt(0)
-    }
-}
-
-# for WSL code
-if ($env:WSL_DISTRO_NAME) {
-    function code-wslg {
-        /usr/share/code/code $args
-    }
-}
-
-# x11
-# for wsl2
-#$env:LIBGL_ALWAYS_INDIRECT=1
-#$env:DISPLAY="$(egrep -m 1 nameserver /etc/resolv.conf | awk '{print $2}'):0.0"
+    -PowerLineFont `
+    -SetCurrentDirectory `
+    -RestoreVirtualTerminal `
+    -HideError `
+    -Colors "#2b2b2b", "#2b2b2b", "#2b2b2b", "#2b2b2b", "#2b2b2b", "#187823", "#600094", "#2b2b2b"
 
 # maintain the "explorer ." muscle memory
 if ($Global:IsLinux) {
     if (-not $env:WSL_DISTRO_NAME) {
         function explorer { dde-file-manager $args }
-    } else {
-        function explorer { wslview $args }
     }
-} else {
-    function explorer { explorer.exe $args }
+    else {
+        # wsl
+
+        <#
+        .SYNOPSIS
+            Call the local /usr/share/code/code instead the Windows remote one
+        #>
+        function codewsl {
+            /usr/share/code/code $args
+        }
+
+        <#
+        .SYNOPSIS
+            For wsl we call the Windows Explorer interop
+        #>
+        function explorer { wslview $args }
+
+        # start ssh-agent for WSL add to /etc/profile
+        # eval $(ssh-agent -s)
+
+        # add the key
+        # ssh-add 
+
+        # start Xforward
+        # ssh -fT castello@$DELL_SERVER sleep infinity
+    }
+
+    # linux enviroment
+    # Linux environment
+    # enable color support of ls and also add handy aliases
+    if ( Test-Path -Path /usr/bin/dircolors ) {
+        test -r ~/.dircolors && $(dircolors -b ~/.dircolors | Out-Null) || $(dircolors -b | Out-Null)
+        
+        function l {
+            /usr/bin/ls --color=auto $args
+        }
+        #alias dir='dir --color=auto'
+        #alias vdir='vdir --color=auto'
+
+        # function grep {
+        #     /usr/bin/grep --color=auto $args
+        # }
+        function fgrep {
+            /usr/bin/fgrep --color=auto $args
+        }
+        function egrep {
+            /usr/bin/egrep --color=auto $args
+        }
+    }
+
+    # colored GCC warnings and errors
+    #$env:GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+    # some more ls aliases
+    function ll { ls -alF $args }
+    function la { ls -A $args }
+    function lcf { ls -CF $args }
+
+    # bash completation?
+    # Install-Module -Name "PSBashCompletions"
+    function __completation ($command) {
+        Register-BashArgumentCompleter "$command" /usr/share/bash-completion/completions/$command
+    }
+
+    # register on demand
+    Set-PSReadLineKeyHandler -Key Spacebar -ScriptBlock {
+        $line = $cursor = $null
+        
+        # get the command
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref] $line, [ref] $cursor)
+        # the key handled must be outputed
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert(' ')
+        
+        # check if we have the bash complestions mathc
+        try {
+            __completation $line   
+        }
+        catch {
+            # nothing to do here    
+        }
+    }
+
+    # x11
+    # for wsl2
+    #$env:LIBGL_ALWAYS_INDIRECT=1
+    #$env:DISPLAY="$(egrep -m 1 nameserver /etc/resolv.conf | awk '{print $2}'):0.0"
+
+    # set the default theme
+    #$env:THEME=$(wslsys -t)
+    #$env:GTK_THEME="Pop"
+
+    #if ( $THEME -match "dark" ) {
+    #    $env:GTK_THEME="Pop-dark"
+    #}
+
+    # NUTTX
+    #$env:PATH="/opt/gcc/gcc-arm-none-eabi-10-2020-q4-major/bin:$PATH"
+    $env:PATH = "/opt/gcc/gcc-arm-none-eabi-9-2019-q4-major/bin:$env:PATH"
+    $env:PICO_SDK_PATH = "/home/castello/tmp/pico-sdk"
+    $env:PATH = "/opt/gcc/riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-linux-ubuntu14/bin:$env:PATH"
+    #$env:PATH="/opt/gcc-riscvm32/riscv32-esp-elf/bin:$PATH"
+
+    # ESPTOOL
+    $env:PATH = "/home/castello/.local/bin:$env:PATH"
+
+    # rustup
+    $env:PATH = "/home/castello/.cargo/bin:$env:PATH"
+
+    # go
+    $env:PATH = "/usr/local/go/bin:$env:PATH"
+
+    # set USERPROFILE
+    $env:USERPROFILE = $env:HOME
 }
+else {
+    # Microsoft Windows
 
-# set the default theme
-#$env:THEME=$(wslsys -t)
-#$env:GTK_THEME="Pop"
+    function explorer { explorer.exe $args }
 
-#if ( $THEME -match "dark" ) {
-#    $env:GTK_THEME="Pop-dark"
-#}
-
-# NUTTX
-#$env:PATH="/opt/gcc/gcc-arm-none-eabi-10-2020-q4-major/bin:$PATH"
-$env:PATH="/opt/gcc/gcc-arm-none-eabi-9-2019-q4-major/bin:$env:PATH"
-$env:PICO_SDK_PATH="/home/castello/tmp/pico-sdk"
-$env:PATH="/opt/gcc/riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-linux-ubuntu14/bin:$env:PATH"
-#$env:PATH="/opt/gcc-riscvm32/riscv32-esp-elf/bin:$PATH"
-
-# ESPTOOL
-$env:PATH="/home/castello/.local/bin:$env:PATH"
-
-# set USERPROFILE
-$env:USERPROFILE=$env:HOME
-
-# start ssh-agent for WSL
-#$(ssh-agent | Out-Null)
+    function Show-Notification-Warn ($title, $text) {
+        Add-Type -AssemblyName System.Windows.Forms 
+        $global:balloon = New-Object System.Windows.Forms.NotifyIcon
+        
+        $path = (Get-Process -id $pid).Path
+        
+        $balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path) 
+        $balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning 
+        $balloon.BalloonTipText = "${text}"
+        $balloon.BalloonTipTitle = "${title}" 
+        
+        $balloon.Visible = $true 
+        $balloon.ShowBalloonTip(5000)
+    }
+}
